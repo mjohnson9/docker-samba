b738247d1202a5fcab6067328c516552fb842c7a
Michael Johnson <nightexcessive@users.noreply.github.com>
2015-11-13 19:04:52 -0500

    Allow LCT to be set by the users config

--- create-users.sh
+++ create-users.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-LCT="$(date +"%s")"
-
 cat /etc/samba/users.conf | while read USERPASS; do
   ID=$(echo $USERPASS | awk '{print $1}')
   USER=$(echo $USERPASS | awk '{print $2}')
   PASS=$(echo $USERPASS | awk '{print $3}')
   SMBPASS=$(echo $USERPASS | awk '{print $4}')
+  LCT=$(echo $USERPASS | awk '{print $5}')
 
   adduser --disabled-login --shell /bin/false --gecos '' --home "/data/homes/$USER" --uid "$ID" --gid 100 "$USER"
   echo "$USER:$PASS" | chpasswd -e
